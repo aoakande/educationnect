112da8f32bb23b295613632a4b7d4ccd1784b875
Olalekan Akande <35519851-aoakande2016@users.noreply.replit.com>
2024-09-27 16:12:34 +0000

    Adding a UI to my blockchain project

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Connect } from '@stacks/connect-react';
+import { UserSession, AppConfig } from '@stacks/auth';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Institution from './pages/Institution';
+import Student from './pages/Student';
+import Admin from './pages/Admin';
+
+const appConfig = new AppConfig(['store_write', 'publish_data']);
+const userSession = new UserSession({ appConfig });
+
+function App() {
+  return (
+    <Connect userSession={userSession}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/institution" component={Institution} />
+            <Route path="/student" component={Student} />
+            <Route path="/admin" component={Admin} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    </Connect>
+  );
+}
+
+export default App;
